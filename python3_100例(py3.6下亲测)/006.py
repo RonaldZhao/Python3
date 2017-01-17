6c1ecb0a6da4baec30ab8896006bb62c433a662e
gitzzg <zzgdata@163.com>
2017-01-17 12:29:33 +0800

    add e.g.006.py

---
+++ python3_100例(py3.6下亲测)/006.py
@@ -0,0 +1,13 @@
+#!E:\virtualenv\venv\Scripts\python.exe
+# -*- coding:utf-8 -*-
+'''
+【程序6】
+题目：用*号输出字母P的图案。
+1.程序分析：可先用'*'号在纸上写出字母P，再分行输出。
+2.程序源代码：
+'''
+print('*'*12)
+for i in range(6):
+    print('*'+' '*10+'*')
+print('*'*12)
+print('*\n'*8)
